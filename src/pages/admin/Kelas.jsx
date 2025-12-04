3c40d9865944a9cc893fdb75c9d5c8f520c5b34f
irsyad_312310512 <franklnlynirsyad@gmail.com>
2025-12-04 09:42:12 +0700

    fix: reset password flow

--- src/pages/admin/Kelas.jsx
+++ src/pages/admin/Kelas.jsx
@@ -973,7 +973,7 @@ export default function AKelas() {
                 </div>
               </div>
               <div className="mt-4 sm:mt-0">
-                <div className="flex items-center space-x-1 bg-gray-100 rounded-xl p-1 border border-gray-400">
+                <div className="flex items-center space-x-1 bg-gray-300 rounded-xl p-1 border border-gray-400">
                   {[
                     { key: 'kelas', label: 'Kelas & Jadwal', icon: '📚' },
                     { key: 'struktur', label: 'Struktur Sekolah', icon: '🏢' },
