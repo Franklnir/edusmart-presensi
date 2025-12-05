e02812d456603b06f056362ee182450e1e4f2ae2
irsyad_312310512 <franklnlynirsyad@gmail.com>
2025-12-05 10:33:08 +0700

    fix: reset password flow

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
   }
 })
-
